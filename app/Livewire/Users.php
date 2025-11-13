b85343892bb3171148b427b622517fa74094d63c
Wisnu Andika <124800648+pendragonnn@users.noreply.github.com>
2025-11-13 19:46:46 +0700

    mat: actions

--- app/Livewire/Users.php
+++ app/Livewire/Users.php
@@ -3,11 +3,22 @@
 namespace App\Livewire;
 
 use App\Models\User;
+use Hash;
 use Livewire\Component;
 
 class Users extends Component
 {
     public $title = 'User Page';
+
+    public function createUser() 
+    {
+        User::create([
+            'name' => 'User Baru',
+            'email' => 'putra@email.com',
+            'password' => Hash::make('password')
+        ]);
+    }
+
     public function render()
     {
         return view('livewire.users', [
--- resources/views/livewire/users.blade.php
+++ resources/views/livewire/users.blade.php
@@ -1,4 +1,15 @@
-<div>
+<div class="w-1/2 m-auto my-10">
     <h1 class="text-3xl font-semibold">{{ $title }}</h1>
     <p>User Count: {{ count($users) }}</p>
+    <button wire:click="createUser" type="button" class="text-white bg-blue-500 hover:bg-blue-800 font-medium rounded-md text-sm px-5 p-2.5 cursor-pointer">Create User</button>
+
+    <hr class="border-1 my-5">
+
+    <h2 class="text-2xl font-semibold">Users List</h2>
+    <ul class="list-disc">
+        @foreach ($users as $user)
+            <li>{{ $user->name }}</li>
+        
+        @endforeach
+    </ul>
 </div>
